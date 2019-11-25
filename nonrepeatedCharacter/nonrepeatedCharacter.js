8636ac452f84d3f879f2e3e1235b4f565c307dc9
Ahmed Wheida <ahmed.wheida@gmail.com>
2019-11-25 08:59:44 +0100

    used objects to make sure there are non-repeating counters

--- nonrepeatedCharacter/nonrepeatedCharacter.js
+++ nonrepeatedCharacter/nonrepeatedCharacter.js
@@ -7,5 +7,35 @@
  */
 
 var firstNonRepeatedCharacter = function(string) {
-  // TODO: your solution here
+
+	//loop through the string
+	// count how many times each is repeated
+	// break after the finding one
+
+	var arr = string.split('');
+
+	var acc = {};
+
+	// initiate unique counters for each letter
+
+	for (var i = 0; i < arr.length; i++) {
+		acc[arr[i]] = 0
+	}
+
+
+	// increment each value corrosponding to a key
+
+	for (var i = 0; i < arr.length; i++) {
+		acc[arr[i]] = acc[arr[i]] + 1
+	}
+
+
+	for(var key in acc){
+		if (acc[key]===1) {
+			return key
+		}
+	}
+
+
+
 };
